5096cc1d8a881a840ad6364eb8fe766df29adf8a
senabyrmgl <sena.bayramoglu13@icloud.com>
2021-12-31 11:02:17 +0300

    first commit

---
+++ src/main/java/com/mycompany/aractakipoto/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+    //Programı çalıştırdığımız class şuan için sadece login classınının objesini yaratıp formun görünür olmasını sağlıyoruz
+    public static void main(String[] args) {
+        login a = new login();
+        a.setVisible(true);
+        
+    }
+}
